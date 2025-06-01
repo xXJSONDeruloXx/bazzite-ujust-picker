5d424e2228da9df334e524905691827e507a8d20
xXJSONDeruloXx <danielhimebauch@gmail.com>
2025-05-31 22:16:15 -0400

    fix: corrected colors

--- picker.go
+++ picker.go
@@ -39,9 +39,9 @@ var (
 	layoutWidth    = 72
 	appBorder      = lipgloss.NewStyle().Border(lipgloss.NormalBorder()).Width(layoutWidth)
 	titleStyle     = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#faebd7"))                           // Bazzite white
-	catSelected    = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#00afff")).Underline(true)           // Bazzite blue
+	catSelected    = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#acd7e6")).Underline(true)           // Bazzite light blue
 	catInactive    = lipgloss.NewStyle().Foreground(lipgloss.Color("#a8a8a8"))                                      // Bazzite dark grey
-	recipeActive   = lipgloss.NewStyle().Foreground(lipgloss.Color("#ff1492")).Bold(true)                           // Bazzite pink/magenta
+	recipeActive   = lipgloss.NewStyle().Foreground(lipgloss.Color("#ff14ff")).Bold(true)                           // Bazzite purple/magenta
 	recipeInactive = lipgloss.NewStyle().Foreground(lipgloss.Color("#a8a8a8"))                                      // Bazzite dark grey
 	descText       = lipgloss.NewStyle().Padding(1, 0).Width(layoutWidth - 4).Foreground(lipgloss.Color("#faebd7")) // Bazzite white
 	controlStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("#a8a8a8")).Italic(true)                         // Bazzite dark grey
